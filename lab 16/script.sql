44900703bba83cc65136b96b59e54e18e0054a7b
XimePerezEscalante <A01751827@tec.mx>
2025-03-16 23:59:39 -0600

    Nueva consulta

--- lab 16/script.sql
+++ lab 16/script.sql
@@ -3,11 +3,18 @@
 /* Obtener la descripción de los materiales que no han sido entregados */
 
 SELECT Descripcion
-FROM Materiales M
+FROM Materiales
 WHERE Clave NOT IN (SELECT Clave FROM Entregan);
 
 /* Obtener los materiales que han sido entregados */
 
-SELECT * 
-FROM materiales, entregan 
+SELECT m.Descripcion 
+FROM materiales m , entregan 
 WHERE materiales.clave = entregan.clave;
+
+/* Obtener los materiales entregados en el 2000 */
+
+SELECT m.descripcion, e.fecha
+FROM materiales m, entregan e
+WHERE m.clave = e.clave 
+AND e.fecha LIKE '2000%';

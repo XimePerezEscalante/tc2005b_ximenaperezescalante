122eec26a22427ba52da0294383e9fef9f1696c5
XimePerezEscalante <A01751827@tec.mx>
2025-03-16 23:56:00 -0600

    Lab 16 no me acuerdo qué consultas teníamos que hacer

--- lab 16/script.sql
+++ lab 16/script.sql
@@ -1,1 +1,13 @@
-/* Consultas */
+/* Consultas, no estoy segura de cuáles se tenían que hacer :o */
+
+/* Obtener la descripción de los materiales que no han sido entregados */
+
+SELECT Descripcion
+FROM Materiales M
+WHERE Clave NOT IN (SELECT Clave FROM Entregan);
+
+/* Obtener los materiales que han sido entregados */
+
+SELECT * 
+FROM materiales, entregan 
+WHERE materiales.clave = entregan.clave;
